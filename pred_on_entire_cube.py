4a991f48a18486b8e35d07cf51547fd1cfb36cae
WangGaige <wanggaige21@126.com>
2023-04-16 16:58:14 +0800

    Update pred_on_entire_cube.py

--- pred_on_entire_cube.py
+++ pred_on_entire_cube.py
@@ -1,27 +1,31 @@
 nparray_pred=nparray.copy()
-
+nparray_1=nparray.copy()
 import time
 import matplotlib.image as im
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 size=128
 maxv_cut=1200
-for line in tqdm(nparray.shape[0]):
-    seisline =nparray[line,:,:]
-    print("{}-{}".format(line,nparray.shape[0]))
-    
-    maxv=maxv_cut
-    maxcondition=(seisline>=max)
-    a=np.where(maxcondition)
-    minv=-maxv_cut
-    mincondition=(seisline<=min)
-    b=np.where(mincondition)
-    seisline[a]=maxv_cut
-    seisline[b]=-maxv_cut
-    
-    seisline=np.interp(seisline, (seisline.min(), seisline.max()), (0, uni_clr.shape[0]-1))
-    seisline=seisline.astype(dtype='int64')
-    seisline=uni_clr[seisline.transpose()]
+
+maxv=maxv_cut
+maxcondition=(nparray_1>=maxv)
+a=np.where(maxcondition)
+minv=-maxv_cut
+mincondition=(nparray_1<=minv)
+b=np.where(mincondition)
+nparray_1[a]=maxv_cut
+nparray_1[b]=-maxv_cut
+nparray_1=np.interp(nparray_1, (nparray_1.min(),nparray_1.max()), (0, uni_clr.shape[0]-1))
+nparray_1=nparray_1.astype(dtype='int64')
+
+nparray_1=uni_clr[nparray_1]
+
+
+import scipy.ndimage as ndimage
+for line in tqdm(range(nparray_1.shape[0])):
+    seisline =nparray_1[line,:,:]
+    #print("{}-{}".format(line,nparray.shape[0]))
+    seisline=seisline.transpose(1,0,2)
     seisline=seisline.astype(np.uint8)
 
     iblock=seisline.shape[0]//size
@@ -30,8 +34,8 @@ for line in tqdm(nparray.shape[0]):
 
     for i in range(i_block+1):
         for j in range(j_block+1):
-            ll=x0[i*size:(i+1)*size,j*size:(j+1)*size]
-            ll = np.array(ll, dtype='uint8')
+            ll=seisline[i*size:(i+1)*size,j*size:(j+1)*size]
+            
             x = cv2.resize(ll, (W, H))
             x = x / 255.0
             x = x.astype(np.float32)
@@ -40,6 +44,7 @@ for line in tqdm(nparray.shape[0]):
             p = np.expand_dims(p, axis=-1)
             p = p * (255/num_classes)
             p = p.astype(np.uint8)
+            p = ndimage.gaussian_filter(p, sigma=(2, 2, 2), order=0)
             p = cv2.resize(p, (size,size))
             cavepred[i*size:(i+1)*size,j*size:(j+1)*size,0]=p
     cavepred=cavepred[0:x0.shape[0],0:x0.shape[1],:]
